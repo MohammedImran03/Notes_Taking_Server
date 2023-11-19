89c1cc22f1cd1ebdebfbcdb6eac552c79314b8c9
MohammedImran03 <mohammedimran1407.mi@gmail.com>
2023-11-19 23:37:03 +0530

    Notes_Taking_App_Backend

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 
 app.use(cors({
-  origin: ['http://localhost:3000',],
+  origin: ['https://quickmemo-notes.netlify.app/',],
   credentials: true
 }));
 // app.use(cors());
